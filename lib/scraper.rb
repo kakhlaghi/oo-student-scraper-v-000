d0bfd9fd52cb9174e3a29e845b663ed38975cb37
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-17 00:26:37 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,25 +4,26 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+
     scrape_hash = []
     html = open(index_url)
     doc = Nokogiri::HTML(html)
     doc.css("div.roster-cards-container").each do |roster_card| roster_card.css('.student-card a').each do
-    |student| 
+    |student|
       scrape_name = student.css(".student-name").text
       scrape_location = student.css(".student-location").text
       scrape_url = student["href"]
-      
+
       scrape_hash << {:name => scrape_name, :location => scrape_location, :profile_url => scrape_url}
-    
+
     end
     end
     scrape_hash
   end
 
   def self.scrape_profile_page(profile_url)
-    profile_hash = Hash.new 
+    profile_hash = Hash.new
+    binding.pry
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
     doc.css("div.social-icon-container a").collect do |icon|
@@ -36,10 +37,10 @@ class Scraper
         profile_hash[:blog] = icon["href"]
       end
     end
-  
+
     profile_hash[:profile_quote] = doc.css("div.profile-quote").text
     profile_hash[:bio] = doc.css("div.description-holder p").text
-    
+
     profile_hash
   end
 end
